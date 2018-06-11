4d15ff862b6cc8b640433b2503fe351b248709bc
Paul Hafer <phafer@mail.usf.edu>
2018-06-11 12:57:37 -0400

    Initial commit

---
+++ Vidly/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        // Data Annotations - Name attribute is not nullable
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        // Navigation property
+        public MembershipType MembershipType { get; set; }
+        // Entity treats this as foreign key
+        public byte MembershipTypeId { get; set; }
+    }
+}
